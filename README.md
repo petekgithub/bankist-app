07a88068106fb4baacee1e0a35a5bb003ce12b5c
Petech <petekshamitbeyli@gmail.com>
2024-04-16 14:52:14 +0300

    transfer-money-readme-updates

---
+++ README.md
@@ -0,0 +1,1 @@
+![Örnek Resim](./assets/bank.png)
--- script.js
+++ script.js
@@ -169,3 +169,27 @@ btnLogin.addEventListener("click", function (e) {
     updateUI(currentAccount);
   }
 });
+
+//** transfer money btn */
+btnTransfer.addEventListener("click", function (e) {
+  e.preventDefault();
+  const amount = Number(inputTransferAmount.value);
+  const receiverAcc = accounts.find(
+    (acc) => acc.username === inputTransferTo.value
+  );
+  inputTransferAmount.value = inputTransferTo.value = "";
+
+  if (
+    amount > 0 &&
+    receiverAcc &&
+    currentAccount.balance >= amount &&
+    receiverAcc?.username !== currentAccount.username
+  ) {
+    // Doing the transfer
+    currentAccount.movements.push(-amount);
+    receiverAcc.movements.push(amount);
+
+    // Update UI
+    updateUI(currentAccount);
+  }
+});
